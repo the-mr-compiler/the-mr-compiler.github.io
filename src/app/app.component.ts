aed3f6263849d720ab083fee7e93b9521802b0bf
Meghanath Nalawade <50794140+mr-compilerMS@users.noreply.github.com>
2022-12-24 12:24:38 +0530

    updated manifest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'megh-portfolio';
   isMenuOpen = false;
   menus = [
     { name: 'Home', link: '#home-section', active: true, offset: 0 },
